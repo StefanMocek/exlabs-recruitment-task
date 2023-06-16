8e7947607aa60d9b9b58fd78201ae876b20c9766
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-06-16 20:22:20 +0200

    RequestValidationError applied

--- src/utils/middlewares/validate-request.ts
+++ src/utils/middlewares/validate-request.ts
@@ -1,13 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 import { validationResult } from 'express-validator';
+import {RequestValidationError} from '../errors';
 
 export const validateRequest = async (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
 
     if(!errors.isEmpty()) {
-        //todo 
-        //request validation error
-        return next()
+        return next(new RequestValidationError(errors.array()))
     };
 
     next();
