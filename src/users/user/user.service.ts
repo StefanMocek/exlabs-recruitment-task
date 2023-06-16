34832bc931c554108daec433b74364e51cf98f60
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-06-16 23:26:35 +0200

    user service and dtos

---
+++ src/users/user/user.service.ts
@@ -0,0 +1,53 @@
+import {UserModel, User} from './user.model';
+import {CreateUserDto, UpdateUserDto, DeleteUserDto} from '../dtos/user.dto';
+
+export class UserService {
+  constructor(public userModel: UserModel) { };
+
+  async getOneById(userId: string) {
+    return await this.userModel.findOne({_id: userId});
+  };
+
+  async getAllUsers(role: string) {
+    return await this.userModel
+      .find({role});
+  };
+
+  async create(createUserDto: CreateUserDto) {
+    const user = new this.userModel({
+      firstName: createUserDto.firstName || '',
+      lastName: createUserDto.lastName || '',
+      email: createUserDto.email,
+      role: createUserDto.role
+    });
+
+    return await user.save();
+  };
+
+  async update(updateUserDto: UpdateUserDto) {
+    const updateFields: any = {};
+
+    if (typeof updateUserDto.firstName !== 'undefined') {
+      updateFields.firstName = updateUserDto.firstName;
+    };
+
+    if (typeof updateUserDto.lastName !== 'undefined') {
+      updateFields.lastName = updateUserDto.lastName;
+    };
+
+    if (typeof updateUserDto.role !== 'undefined') {
+      updateFields.role = updateUserDto.role;
+    };
+    
+    return await this.userModel.findOneAndUpdate(
+      {_id: updateUserDto.userId},
+      {$set: {...updateFields}},
+      {new: true})
+  }
+
+  async delete(deleteUserDto: DeleteUserDto) {
+    return await this.userModel.findOneAndRemove({_id: deleteUserDto.userId})
+  };
+};
+
+export const userService = new UserService(User);
