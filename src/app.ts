3054d35cbb2a2634fe386c5d4b67dde02c97ff8a
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-06-17 13:26:08 +0200

    swagger docs applied - localy

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import {AppModule} from "./app.module";
 import {JwtPayload} from './utils/globals';
@@ -10,13 +11,15 @@ declare global {
   }
 };
 
+const swaggerDocPath = path.join(__dirname, '../../swagger.yaml')
+
 const boostrap = async () => {
   //check if JWT_KEY is present if not app wont start
   //real mongo DB will be connected here for tests purpose
   if (!process.env.MONGO_URL) {
     throw new Error('MONGO_URL is require')
   };
-  const app = new AppModule(express(), process.env.MONGO_URL);
+  const app = new AppModule(express(), process.env.MONGO_URL, swaggerDocPath);
   await app.start();
 };
 
